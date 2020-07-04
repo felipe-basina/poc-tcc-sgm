d051640bb28efbc1ec02073c4e94eb7c8480ab06
fnascimento <felipe.basina@gmail.com>
2020-07-04 11:10:20 -0300

    Adicionado diretorio e projeto para api,

---
+++ api/safim-api/src/main/java/poc/tcc/sgm/SafimApiApplication.java
@@ -0,0 +1,13 @@
+package poc.tcc.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafimApiApplication.class, args);
+	}
+
+}
