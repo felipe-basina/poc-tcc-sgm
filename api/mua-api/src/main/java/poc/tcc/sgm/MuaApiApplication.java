83a83dc931c444597f1e83a579374571c8fefcdd
fnascimento <felipe.basina@gmail.com>
2020-07-05 12:12:39 -0300

    Adicionado projeto modulo de usuarios da aplicacao.

---
+++ api/mua-api/src/main/java/poc/tcc/sgm/MuaApiApplication.java
@@ -0,0 +1,13 @@
+package poc.tcc.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuaApiApplication.class, args);
+	}
+
+}
---
+++ api/mua-api/src/test/java/poc/tcc/sgm/MuaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.tcc.sgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
