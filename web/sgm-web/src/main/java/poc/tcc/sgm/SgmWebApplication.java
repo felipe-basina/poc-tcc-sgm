2f295d242dfff2127b2e15c69a00e5cb2456f246
fnascimento <felipe.basina@gmail.com>
2020-07-06 18:09:23 -0300

    Adicionado estrutura para projeto web responsivo.

---
+++ web/sgm-web/src/main/java/poc/tcc/sgm/SgmWebApplication.java
@@ -0,0 +1,13 @@
+package poc.tcc.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmWebApplication.class, args);
+	}
+
+}
