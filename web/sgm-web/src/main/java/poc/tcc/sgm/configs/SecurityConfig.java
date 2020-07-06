c10c56ecbc9294f1866441f152eb193187781070
fnascimento <felipe.basina@gmail.com>
2020-07-06 20:00:59 -0300

    Adicionado estrutura + layout do projeto.

---
+++ web/sgm-web/src/main/java/poc/tcc/sgm/configs/SecurityConfig.java
@@ -0,0 +1,56 @@
+package poc.tcc.sgm.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import poc.tcc.sgm.components.LoggingAccessDeniedHandler;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoggingAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.authorizeRequests()
+                    .antMatchers(
+                    		"/assets/**",
+                            "/js/**",
+                            "/css/**",
+                            "/img/**",
+                            "/webjars/**",
+                            "/register").permitAll()
+                    //.antMatchers("/user/**").hasRole("USER")
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                .logout()
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+                .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(this.accessDeniedHandler);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("aflores").password("{noop}Senha123").roles("MUNICIPE")
+            .and()
+                .withUser("amelia").password("{noop}Senha123").roles("SERVIDOR");
+    }
+	
+	
+}
---
+++ web/sgm-web/src/main/java/poc/tcc/sgm/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package poc.tcc.sgm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+	
+}
