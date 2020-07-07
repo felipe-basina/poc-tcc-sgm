f84f686ebb87295974573efcb0f0db3a9eac9854
fnascimento <felipe.basina@gmail.com>
2020-07-07 09:20:47 -0300

    Removido classe de configuracao e controle para acesso negado.

--- web/sgm-web/src/main/java/poc/tcc/sgm/configs/SecurityConfig.java
+++ web/sgm-web/src/main/java/poc/tcc/sgm/configs/SecurityConfig.java
@@ -1,19 +1,13 @@
 package poc.tcc.sgm.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import poc.tcc.sgm.components.LoggingAccessDeniedHandler;
-
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    //@Autowired
-    //private LoggingAccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,9 +34,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .logoutSuccessUrl("/login?logout")
                     .permitAll();
-                //.and()
-               // .exceptionHandling()
-                //    .accessDeniedHandler(this.accessDeniedHandler);
     }
 
     @Override
@@ -52,6 +43,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .withUser("amelia").password("{noop}Senha123").roles("SERVIDOR");
     }
-	
 	
 }
